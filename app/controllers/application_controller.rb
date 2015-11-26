01b0d49db2650860da5e3142d2b6dd927173b63e
peterortiz82 <peterortiz82@gmail.com>
2015-11-26 12:03:54 -0500

    fixing login issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   before_filter :configure_permitted_parameters, if: :devise_controller?
   before_filter :invitaion_permitted_parameters, if: :devise_controller?
-  before_filter :set_mailer_host
   protect_from_forgery with: :exception
 
   protected
-
-  def set_mailer_host
-    ActionMailer::Base.default_url_options[:host] = request.host_with_port
-  end
 
   def invitaion_permitted_parameters
     devise_parameter_sanitizer.for(:accept_invitation) do |u|
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'expensify.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'www.expense-pro.com' }
 end
