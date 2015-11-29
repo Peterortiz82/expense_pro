4b6b672ce18e1c8528e5dc97e7039eed2191d0c7
peterortiz82 <peterortiz82@gmail.com>
2015-11-28 21:10:42 -0500

    Added Fast Food to the Food Category

---
+++ db/migrate/20151129020637_add_fastfood_to_category.rb
@@ -0,0 +1,6 @@
+class AddFastfoodToCategory < ActiveRecord::Migration
+  def change
+    food = Category.find_by name: 'Food'
+    SubCategory.create name: 'Fast Food', category_id: food.id
+  end
+end
