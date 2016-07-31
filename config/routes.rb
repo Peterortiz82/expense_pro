0482f9bb7ef4cf6f0fa75d4c28fa8b0bfdf307d2
peterortiz82 <peterortiz82@gmail.com>
2016-07-31 14:00:46 -0400

    generated custom error pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "welcome#index"
 
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
+
   devise_for :users
 
   resources :lists do
