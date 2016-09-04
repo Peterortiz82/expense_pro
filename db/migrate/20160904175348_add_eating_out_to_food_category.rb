690d08571ce1189b126c8ba0fe0e97e53c336961
peterortiz82 <peterortiz82@gmail.com>
2016-09-04 16:24:07 -0400

    removed id from migration

--- db/migrate/20160904175348_add_eating_out_to_food_category.rb
+++ db/migrate/20160904175348_add_eating_out_to_food_category.rb
@@ -1,14 +1,16 @@
 class AddEatingOutToFoodCategory < ActiveRecord::Migration
 
   def up
-    SubCategory.create!(name: "Breakfast", category_id: 5)
-    SubCategory.create!(name: "Lunch", category_id: 5)
-    SubCategory.create!(name: "Dinner", category_id: 5)
+    category = Category.find_by(name: "Food")
+
+    SubCategory.create!(name: "Breakfast", category_id: category.id)
+    SubCategory.create!(name: "Lunch", category_id: category.id)
+    SubCategory.create!(name: "Dinner", category_id: category.id)
   end
 
   def down
     sub_category = %w(Breakfast Lunch Dinner)
-    category = Category.find 5
+    category = Category.find_by(name: "Food")
 
     category.sub_categories.each do |sub|
       sub.destroy if sub_category.include?(sub.name)
